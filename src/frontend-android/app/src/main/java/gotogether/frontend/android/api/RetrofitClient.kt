5cdfe8a5ab634ddb030a4534a027907d5020f229
Simon Völkl <s.voelkl2@oth-aw.de>
2026-06-25 17:33:06 +0200

    Fix ip

--- src/frontend-android/app/src/main/java/gotogether/frontend/android/api/RetrofitClient.kt
+++ src/frontend-android/app/src/main/java/gotogether/frontend/android/api/RetrofitClient.kt
@@ -6,7 +6,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
-    private const val BASE_URL = "http://172.19.0.1:8080/api/"
+    private const val BASE_URL = "http://192.168.0.1:8080/api/"
 
     private val logging = HttpLoggingInterceptor().apply {
         level = HttpLoggingInterceptor.Level.BODY
